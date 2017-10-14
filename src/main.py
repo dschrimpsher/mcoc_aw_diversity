12de9c86a02c0e40687607ca01d561b5b6273474
Dan Schrimpsher <dan.schrimpsher@gmail.com>
2017-10-13 23:46:34 -0500

    Added bg3

--- src/main.py
+++ src/main.py
@@ -11,6 +11,11 @@ bg2.load('data/bg2.json')
 bg2.optimize()
 print(bg2)
 
+bg3 = bg.BG()
+bg3.load('data/bg3.json')
+bg3.optimize()
+print(bg3)
+
 # test = player.Player()
 # test.load('data/drdew.json')
 # print(test)
